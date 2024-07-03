a7bf3548c521567bea10870425cdb8f69ff9720d
Laptop <jdkyaz@gmail.com>
2024-07-02 17:35:14 -0700

    Changed the client's AUth file to have default export

--- Develop/client/src/utils/auth.js
+++ Develop/client/src/utils/auth.js
@@ -1,31 +1,31 @@
-const jwt = require('jsonwebtoken');
+class AuthService {
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
 
-const secret = 'your-secret-key';
-const expiration = '2h';
-
-module.exports = {
-  authMiddleware: function ({ req }) {
-    let token = req.headers.authorization || '';
-
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return req;
-    }
-
+  isTokenExpired(token) {
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
+      const decoded = jwt.decode(token);
+      return decoded.exp < Date.now() / 1000;
+    } catch (err) {
+      return false;
     }
+  }
 
-    return req;
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-};
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
